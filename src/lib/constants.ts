8fe9c81ed1bb7127efdea00c0063066532673317
Pablo Cappellacci <pablocappe@gmail.com>
2025-06-02 01:07:18 -0300

    BA - Git commit message template

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const BLOG_PATH = "efimeros";
-export const WTF_IS_IT = "un poco de todo";
+export const BLOG_PATH = "Creo, luego analizo";
+export const WTF_IS_IT = "Analista de Negocios, con orientación a la creatividad y la innovación.";
 export const HOME_OG_IMAGE_URL =
   "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
