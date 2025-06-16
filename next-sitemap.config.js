a4bc85a39ce5f8cf35ece40af94b846894da4f72
Pablo Cappellacci <pablocappe@gmail.com>
2025-06-16 01:01:01 -0300

    SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://pabloc.dev.ar',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({
   return (
     <html lang="es-419" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="7XcQh-WbTqNMTk990YIrcmDXVdm2oICfmbh8czz6kNM" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
